b4f73ab65d99a7cbf2fd2c2cc499d69501778cd8
seba-8 <56670136+seba-8@users.noreply.github.com>
2021-02-21 15:05:34 -0500

    Update CdRocket.m

--- Descent Program/CdRocket.m
+++ Descent Program/CdRocket.m
@@ -9,17 +9,17 @@ function [Cd, S] = CdRocket(h, U, Ma)
 	
 	% approach #1 using (Mach << 1) Cp approximation
 	Cp = 1.28; 				% Cd for a flat plate perpendicular to flow
-	Pfront = 2*Cp*rho(h)*(U^2);
+	Pfront = 0.5*Cp*rho(h)*(U^2);
 	% approach #2 using reynolds momentum transport theorem on a "streamline" over the nosecone (assumes slip condition, Mach << 1)
 	v1x = U;
 	v1y = 0;
 	theta = atan(R/L_nosecone);
 	v2x = U*cos(theta);
 	v2y = U*sin(theta);
-	FoverA(1) = (v1x^2)*rho(h) - (v2x^2)*rho(h);	% reynolds transport formula without the area term (later accounted for)
-	FoverA(2) = (v1y^2)*rho(h) - (v2y^2)*rho(h);
-	Aratio = sin(theta);				% ratio of control volume's dA (surface perpendicular to flow and touching the tip of the nosecone) to the corresponding nosecone surface area spanned by the control volume.
-	P = FoverA*Aratio;				% F/A on the nosecone [x,y]
+	FoverA(1) = abs((v1x^2)*rho(h) - (v2x^2)*rho(h));	% reynolds transport formula without the area term (later accounted for)
+	FoverA(2) = abs((v1y^2)*rho(h) - (v2y^2)*rho(h));
+	Aratio = sin(theta);					% ratio of control volume's dA (surface perpendicular to flow and touching the tip of the nosecone) to the corresponding nosecone surface area spanned by the control volume.
+	P = FoverA*Aratio;					% F/A on the nosecone [x,y]
 	Pfront = P(1)*sin(theta) + P(2)*cos(theta);
 	Pfront = Pfront / sqrt(1 - Ma^2);		% mach adjustment
 
